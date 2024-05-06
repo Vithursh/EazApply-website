f3810ae9c80eddb44e3ec7c6163e91d95fcb58de
Vithursh Thananchayan <Vithursh0512@gmail.com>
2024-05-06 01:58:05 -0400

    Created login, register pages, the navbar and deleted useless files

---
+++ frontend/src/components/LoginPage.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/LoginPage.css'
+import RegisterPage from './RegisterPage';
+import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
+
+const LoginPage: React.FC = () => {
+    return (
+        <div className="bg-dark text-dark min-vh-100 d-flex align-items-center">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label className="register-label">Login</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="lname">Email</label>
+                                        <input type="text" id="lname" name="lname" className="form-control custom-width"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="lname">Password</label>
+                                        <input type="text" id="lname" name="lname" className="form-control custom-width"/>
+                                    </div>
+                                    <br></br>
+                                    <button className="btn btn-primary">Login</button>
+                                    <Link to='/register'>Don't have an account?</Link>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ frontend/src/components/RegisterPage.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/RegisterPage.css'
+import LoginPage from './LoginPage';
+import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
+
+const RegisterPage: React.FC = () => {
+    return (
+        <div className="bg-dark text-dark min-vh-100 d-flex align-items-center">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label className="register-label">Register</label>
+                                        <br></br>
+                                        <label htmlFor="fname">Name</label>
+                                        <input type="text" id="fname" name="fname" className="form-control custom-width"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="lname">Email</label>
+                                        <input type="text" id="lname" name="lname" className="form-control custom-width"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="lname">Password</label>
+                                        <input type="text" id="lname" name="lname" className="form-control custom-width"/>
+                                    </div>
+                                    <br></br>
+                                    <button className="btn btn-primary">Register</button>
+                                    <Link to='/login'>Already have an account?</Link>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterPage;
