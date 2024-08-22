80c540f997598943f558d9a9116e8c1e1ad43120
Vithursh Thananchayan <Vithursh0512@gmail.com>
2024-08-22 15:54:51 -0400

    Implemented the BFS algorithm to vist each link in the books.toscrape website

--- backend/Search Engine/WebCrawler/WebCrawler/spiders/EazApplybot.py
+++ backend/Search Engine/WebCrawler/WebCrawler/spiders/EazApplybot.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from bs4.element import Tag
 from urllib.parse import urljoin
 from functools import partial
+from collections import deque
+import pandas as pd
 import os
 import shutil
 import re
@@ -12,16 +14,17 @@ from scrapy.crawler import Crawler
 class EazApplySpider(scrapy.Spider):
     name = 'EazApplybot'
     start_urls = ['http://books.toscrape.com/']
-    sub_urls = []
-    crawled_urls = []
+    sub_urls = deque(start_urls)
+    crawled_urls = set()
 
     raw_pages_path = "Raw HTML Pages"
     indexed_pages = "Indexed HTML Pages"
 
-    switch_to_sub_urls = False
+    csv_urls = []
 
-    global total_score
+    new_links = []
 
+    # Words to match to
     job_terms = {
             "Annual leave": 0,
             "Applicant tracking system (ATS)": 0,
@@ -75,12 +78,9 @@ class EazApplySpider(scrapy.Spider):
             "Zero-hours contract": 0
     }
 
+    # Downloads the seed URL webpages
     def crawl_start_urls(self, response, new_start_urls):
-        # Visit the links in the start url array
-        # print("Went with:", self.switch_to_sub_urls)
-
-        # Get the urls in the "start_urls" array and analyze it
-        
+        # Takes away the "https//"
         page = new_start_urls.split("//")[-1].split("/")[0]
         filename = f'/home/vithursh/Coding/EazApply/backend/File Data/{self.raw_pages_path}/{page}.html'
         
@@ -89,7 +89,7 @@ class EazApplySpider(scrapy.Spider):
             website_name = new_start_urls.split("//")[-1].split(".com")[0]
         elif ".ca" in new_start_urls:
             website_name = new_start_urls.split("//")[-1].split(".ca")[0]
-
+        
         # print("The filename variable contains:",filename,"before it starts scraping the sub links")      
         
         with open(filename, 'wb') as f:
@@ -97,60 +97,86 @@ class EazApplySpider(scrapy.Spider):
         self.log(f'Saved file {filename}')
 
         # print("The URL is:", page,"\n and the url is:", filename)
-        # self.analyzer(response, page, filename, website_name)
+        
+        # Crawler loop
+        yield from self.crawl_loop(response)
 
+
+    # Loops through all links until all webpages have been visited
+    def crawl_loop(self, response):
+        # Get all hyperlinks
+        self.get_hyperlinks(response)
+
+        # Uses the BFS algorithm to vist each link in the books.toscrape website
+        if self.sub_urls:
+            while self.sub_urls:
+                try:
+                    print("The length of sub_urls is:", len(self.sub_urls))
+                    next_url = self.sub_urls.popleft()
+                    self.crawled_urls.add(next_url)
+                    print("The next_url is:", next_url)
+                    self.csv_urls.append(next_url)
+                    yield scrapy.Request(next_url, callback=partial(self.crawl_sub_urls, url=next_url))
+                except Exception as e:
+                    self.logger.error(f"Error processing URL {next_url}: {str(e)}")
+            print("sub_urls has", len(self.sub_urls), "now")
+        else:
+            self.logger.info('All URLs have been processed.')
+            # for links in self.crawled_urls:
+            #     print("Link that has been scraped:", links)
+            
+            df = pd.DataFrame({"Title": self.csv_urls})
+            print(df)
+            df.to_csv('/home/vithursh/Coding/EazApply/backend/File Data/LinkInBooksToScrapeWebsite.csv', index=False)
+
+
+    # Gets all sub URLS in a webpage
+    def get_hyperlinks(self, response):
         # Anchor Tags
         anchor_tags = response.css('a::attr(href)').getall()
-        # for seed_URLS in self.start_urls:
+
+        # Adds the sub links to the sub_urls array
         for anchor_tag in anchor_tags:
             # total_score[3] += self.checkUseful(anchor_tag[0])  # Pass the text of the anchor tag to checkUseful
             official_Link = response.urljoin(anchor_tag)
-            # print("The length is", len(self.sub_urls))
-            if official_Link not in self.sub_urls:
-                # print("The link:", official_Link)
+            # if official_Link not in self.crawled_urls:
+            if official_Link not in self.crawled_urls and official_Link not in self.sub_urls:
                 self.sub_urls.append(official_Link)
+                # print("The link:", official_Link,"is added")
+                # print(f"Added URL: {official_Link}")  # Debugging statement
+
             # Removes repetitive sub-links
             if ".com/index.html" in official_Link:
-                print("Contains '.com/index.html'")
+                # print("Contains '.com/index.html'")
                 if official_Link in self.sub_urls:
                     self.sub_urls.remove(official_Link)
+                    print(official_Link, "is being removed from the sub_urls array")
             # else:
             #     print(official_Link, "will not be put into the seed URL array")
         # print("\n\n\n The urls that are being pushed out:")
 
-        # print("\nAll of the urls in the 'sub_urls' array:")
-        # for i in self.sub_urls:
-        #     print(i)
-
-        for url in self.sub_urls:
-            # If there are URLs to scrape, get the next URL and make a request
-            if self.sub_urls:
-                next_url = self.sub_urls.pop(0)
-                self.crawled_urls.append(next_url)
-                # print("The next url at index 0 is:", next_url)
-                yield scrapy.Request(next_url, callback=partial(self.crawl_sub_urls, url=next_url))
-                # yield scrapy.Request(next_url, callback=self.crawl_sub_urls())
-            else:
-                # If no URLs left to scrape, close the spider
-                self.log('All URLs have been processed.')
+        # for next_page in response.xpath("//a[contains(text(), 'next') or contains(text(), 'Next') or contains(text(), '>>')]/@href").getall():
+            # yield response.follow(next_page, self.get_hyperlinks)
+        #     print("The Xpath is:", next_page)
 
 
+    # Downloads the sub URL webpages
     def crawl_sub_urls(self, response, url):
-        print("The 'url' variable contains:", url)
+        print("The sub url is:", url)
 
         # Get the urls in the "sub_urls" array and analyze it
         page = response.url.split("//")[-1]
-        print("The page contains:", page)
+        # print("The page contains:", page)
 
-        # books.toscrape.com/catalogue/category/books_1/index.html.html
-        url_without_link = " "
+        # Turns "/" to "_" in each link 
+        url_without_link = ""
         for i in page:
             if i == "/":
                 url_without_link = url_without_link + i.replace("/", "_")
             else:
                 url_without_link = url_without_link + i
 
-        print("The new string is:", url_without_link)
+        # print("The new string is:", url_without_link)
 
         # print("The page variable contains:",page)
         filename = f'/home/vithursh/Coding/EazApply/backend/File Data/{self.raw_pages_path}/{url_without_link}'
@@ -163,21 +189,26 @@ class EazApplySpider(scrapy.Spider):
 
         # print("The website_name variable contains:",website_name)  # Outputs: "https://jobs.bell.ca"
 
-        print("The filename variablecontains:",filename)
+        # print("The filename variable contains:",filename)
 
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log(f'Saved file {filename}')
 
-        self.analyzer(response, page, filename, website_name)
+        # print("\nAll of the urls in the 'new_links' array:")
+        # for i in self.new_links:
+        #     print("All the links in the new_links are:",i)
+        
+        yield from self.analyzer(response, page, filename, website_name)
+
 
     # Checks how usefull each tag is by giving it a score based on how much matched words it contains from the list
     def checkUseful(self, tag):
         count = 0
         
-        print("\nThe text should be here:")
+        # print("\nThe text should be here:")
 
-        print("The tag is:",tag)
+        # print("The tag is:",tag)
         
         if tag:  # Check if title exists
             # Checks if it is a BeautifulSoup object
@@ -200,20 +231,23 @@ class EazApplySpider(scrapy.Spider):
                 count += self.job_terms[total_word_score]
                 self.job_terms[total_word_score] = 0
 
-            print(self.job_terms[useful_words])
-            print("The total score is",count)
+            # print(self.job_terms[useful_words])
+            # print("The total score is",count)
         else:
-            print("No tag found.")
+            # print("No tag found.")
             count = 0
         
         return count
 
 
+    # Extracts HTML tags from each webpage
     def analyzer(self, response, URLName, URLFilePathName, website_name):
+        print("The analyzer function has been called!!!")
         with open(URLFilePathName, 'r') as f:
             contents = f.read()
             total_score = [0] * 7
             
+            # Title Tag
             soup = BeautifulSoup(contents, 'html.parser')
             total_score[0] = self.checkUseful(soup.title)
 
@@ -248,7 +282,7 @@ class EazApplySpider(scrapy.Spider):
             count = 0
             for i in total_score:
                 count += i
-            print("The total score for this webpage is:", count)
+            # print("The total score for this webpage is:", count)
 
             # If the score is greater than or equal to 10
             if count > 9:
@@ -261,12 +295,12 @@ class EazApplySpider(scrapy.Spider):
                     # Remove leading and trailing whitespace
                     sanitized_url = sanitized_url.strip()
 
-                    print("the sanitized_url is:", sanitized_url)
+                    # print("the sanitized_url is:", sanitized_url)
 
                     # Move the HTML file to the indexer folder
                     indexedURL = f'/home/vithursh/Coding/EazApply/backend/File Data/{self.indexed_pages}/{website_name}/{sanitized_url}'
                     
-                    print("The indexedURL variable contains:", indexedURL)
+                    # print("The indexedURL variable contains:", indexedURL)
                     
                     # Create the directory if it doesn't exist, but don't include the filename
                     os.makedirs(os.path.dirname(indexedURL), exist_ok=True)
@@ -288,24 +322,19 @@ class EazApplySpider(scrapy.Spider):
                         shutil.move(URLFilePathName, indexedURL)
                     else:
                         print(f"File {indexedURL} already exists, skipping move operation.")
-                    
-                # If it's at the end of the 'sub_urls' array
-                else:
-                    # Create a new folder based on the main URL it's crawling in the 'Raw HTML Pages' folder
-                    # indexedURL = f'/home/vithursh/Coding/EazApply/backend/File Data/{self.indexed_pages}/{URLName}'
-                    # os.makedirs(indexedURL, exist_ok=True)
-                    self.switch_to_sub_urls = False
 
             # Delete an web page
             else:
-                # if not self.sub_urls:
-                #     self.switch_to_sub_urls = False
                 # Delete the HTML page
                 print("The file path that will be deleted is:", URLFilePathName)
                 os.remove(URLFilePathName)
-            print("At the end of the link cycle, it is now:", self.switch_to_sub_urls)
+
+        yield from self.crawl_loop(response)
+        print("The crawl_loop function has been called!!!")
 
 
+    # Starts the crawling process
     def parse(self, response):
+        # Scrapes seed urls
         for url in self.start_urls:
             yield from self.crawl_start_urls(response, url)
